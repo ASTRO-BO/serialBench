4b06e8d40561bb44a47833a13cb74fe34d039064
Andrea Zoli <zoli@iasfbo.inaf.it>
2014-05-08 10:05:38 +0200

    Changes for pubblication.

--- Makefile
+++ Makefile
@@ -1,7 +1,3 @@
-# See README.txt.
-
-.PHONY: all cpp java python clean
-
 all: serialBench
 
 clean:
@@ -16,3 +12,5 @@ protoc_middleman: packet.proto
 serialBench: serialBench.cpp protoc_middleman
 	pkg-config --cflags protobuf  # fails if protobuf is not installed
 	c++ serialBench.cpp packet.pb.cc -o serialBench `pkg-config --cflags --libs protobuf` -lrt
+
+.PHONY: all cpp java python clean
--- serialBench.cpp
+++ serialBench.cpp
@@ -1,3 +1,17 @@
+/***************************************************************************
+    begin                : May 8 2014
+    copyright            : (C) 2014 Andrea Zoli
+    email                : zoli@iasfbo.inaf.it
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software for non commercial purpose              *
+ *   and for public research institutes; you can redistribute it and/or    *
+ *   modify it under the terms of the GNU General Public License.          *
+ *   For commercial purpose see appropriate license terms                  *
+ *                                                                         *
+ ***************************************************************************/
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
